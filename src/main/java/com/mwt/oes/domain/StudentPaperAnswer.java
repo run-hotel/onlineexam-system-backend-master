7ee325d9f5921cac5ea82542d79caf58c4c86a57
wuhufly <1115499597@qq.com>
2022-10-15 15:59:29 +0800

    初始化

---
+++ src/main/java/com/mwt/oes/domain/StudentPaperAnswer.java
@@ -0,0 +1,84 @@
+package com.mwt.oes.domain;
+
+public class StudentPaperAnswer {
+  private Integer answerId;
+
+  private String stuAnswer;
+
+  private String iscorrect;
+
+  private String className;
+
+  private Integer queId;
+
+  private Integer paperId;
+
+  private String sno;
+
+  private String iscollect;
+
+  public Integer getAnswerId() {
+    return answerId;
+  }
+
+  public void setAnswerId(Integer answerId) {
+    this.answerId = answerId;
+  }
+
+
+  public String getClassName() {
+    return className;
+  }
+
+  public void setClassName(String className) {
+    this.className = className == null ? null : className.trim();
+  }
+
+  public String getStuAnswer() {
+    return stuAnswer;
+  }
+
+  public void setStuAnswer(String stuAnswer) {
+    this.stuAnswer = stuAnswer == null ? null : stuAnswer.trim();
+  }
+
+  public String getIscorrect() {
+    return iscorrect;
+  }
+
+  public void setIscorrect(String iscorrect) {
+    this.iscorrect = iscorrect == null ? null : iscorrect.trim();
+  }
+
+  public Integer getQueId() {
+    return queId;
+  }
+
+  public void setQueId(Integer queId) {
+    this.queId = queId;
+  }
+
+  public Integer getPaperId() {
+    return paperId;
+  }
+
+  public void setPaperId(Integer paperId) {
+    this.paperId = paperId;
+  }
+
+  public String getSno() {
+    return sno;
+  }
+
+  public void setSno(String sno) {
+    this.sno = sno == null ? null : sno.trim();
+  }
+
+  public String getIscollect() {
+    return iscollect;
+  }
+
+  public void setIscollect(String iscollect) {
+    this.iscollect = iscollect == null ? null : iscollect.trim();
+  }
+}
